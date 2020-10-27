fce3dc65f3aa2810f6b652b175f13b572f814cef
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-10-26 19:24:54 -0700

    inited with general work

---
+++ practice/carter/src/component/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+   return (
+      <header className='p-3 w-100' style={{backgroundColor: "#3a4152"}}>
+      <h1 className='accent-color'>Notes</h1>
+    </header>
+   )
+}
---
+++ practice/carter/src/component/notes/NotesPage.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function NotesPage() {
+   const [notes, setNotes] = useState([]);
+
+   const apiUrl = "http://localhost:54321/notes/";
+
+   const getNotes = () => {
+      axios
+         .get(apiUrl)
+         .then((res) => {
+            return res.data.map((note) => {
+               setNotes(res.data);
+            });
+         })
+         .catch((err) => {
+            console.error(err);
+         })
+         .then(() => {
+            console.log("finished");
+         });
+   };
+
+   //! return array of notes from backend
+   const checkNotes = () => {
+      // if(document.getElementById('loadNotes').innerText === 'Load Notes'){
+      //       document.getElementById('loadNotes').innerText = 'Hide Notes'
+      getNotes();
+      // document.getElementById('notes').setAttribute('display: initial;')
+      // }else{
+      //    document.getElementById('loadNotes').innerText = 'Load Notes'
+      //    document.getElementById('notes').setAttribute('display: none;')
+      // }
+   };
+
+   // const createNote = () => {
+   //    axios.post(apiUrl, JSON.stringify({ title: "title" }));
+   // };
+
+   return (
+      <div
+         style={{
+            width: "96%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: "0 auto 2% auto",
+         }}
+      >
+         <button
+            id='loadNotes'
+            onClick={checkNotes}
+            className='btn btn-outline-dark'
+            style={{
+               width: "120px",
+               marginRight: "10px",
+               marginBottom: "20px",
+            }}
+         >
+            Load Notes
+         </button>
+         {notes.map((note) => {
+            return (
+               <div
+                  id='notes'
+                  key={note.id}
+                  style={{
+                     padding: "10px",
+                     marginBottom: "10px",
+                     backgroundColor: "#3a4152",
+                     border: "1px solid #1e1e1e",
+                  }}
+               >
+                  <p style={{ fontSize: "24px", fontWeight: "350" }}>
+                     {note.title}
+                  </p>
+                  <p style={{ fontSize: "16px", fontWeight: "250" }}>
+                     {note.content}
+                  </p>
+               </div>
+            );
+         })}
+      </div>
+   );
+}
