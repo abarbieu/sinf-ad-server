fd855d86a5e73eaae6690111c60e88bd307ecfb8
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-11-07 16:19:05 -0800

    SAS-75 fixed postgres connection, pool -> client

--- backend/api/reporting_api/reporting.js
+++ backend/api/reporting_api/reporting.js
@@ -1,38 +1,48 @@
 require("dotenv").config({ path: "../" });
-console.log(process.env)
-const { Pool } = require("pg");
 
-const pool = new Pool();
+const { Client } = require("pg");
+
+var connectionString = process.env.PG_CXN;
+
+const client = new Client({
+   connectionString: connectionString,
+});
+
+client.connect();
 
 const stats_table = process.env.STATS_TABLE || "stats_table";
 const airlines = process.env.AIRLINES || "airlines";
 
 const get = (req, res) => {
-	res.header("Content-Type", "application/json");
-	pool.query(`SELECT * FROM ${airlines}`, (err, result) => {
-		if (err) {
-			res.status(500).json({ err });
-			return;
-		} else {
-			res.status(200).send(result);
-			return;
-		}
-	});
+   res.header("Content-Type", "application/json");
+   console.log("getting me");
+   client.query(`SELECT * FROM airlines`, (err, result) => {
+      console.log("query");
+      if (err) {
+         console.log(err);
+         res.status(400).send(err);
+         return;
+      } else {
+         console.log("success");
+         res.status(200).send(result.rows);
+         return;
+      }
+   });
 };
 
 const createItem = (req, res) => {
-	res.header("Content-Type", "application/json");
+   res.header("Content-Type", "application/json");
 
-	pool.query(
-		`INSERT INTO ${airlines} (Id, Airline,  Abbreviation, Country) VALUES (1 , 'United Airlines', 'UAL', 'USA')`,
-		(err, result) => {
-			if (err) {
-				res.status(500).json({ err });
-				return;
-			} else {
-				res.status(200).send(result);
-			}
-		}
-	);
+   client.query(
+      `INSERT INTO ${airlines} (Id, Airline,  Abbreviation, Country) VALUES (1 , 'United Airlines', 'UAL', 'USA')`,
+      (err, result) => {
+         if (err) {
+            res.status(500).json({ err });
+            return;
+         } else {
+            res.status(200).send(result);
+         }
+      }
+   );
 };
 module.exports = { createItem, get };
