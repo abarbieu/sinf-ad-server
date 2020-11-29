a80385cc3959c908fac50576e45a17d13d16411f
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-11-29 15:39:06 -0800

    Style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,40 @@
-import './App.css'
-import FlowGrid from './component/FlowGrid'
-import AdTemplate from './component/AdTemplate.jsx'
-import AdGrid from './component/AdGrid.jsx'
-import NavBar from './component/NavBar.jsx'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import DetailedView from './component/DetailedView.jsx'
-import ShowReport from './component/ShowReport.jsx'
-import EditView from './component/EditView.jsx'
+import "./App.css";
+import FlowGrid from "./component/FlowGrid";
+import AdTemplate from "./component/AdTemplate.jsx";
+import AdGrid from "./component/AdGrid.jsx";
+import NavBar from "./component/NavBar.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DetailedView from "./component/DetailedView.jsx";
+import ShowReport from "./component/ShowReport.jsx";
+import EditView from "./component/EditView.jsx";
 
 function App() {
-  const notFound = () => { return (<div><h1>404 page not found</h1></div>)}
+   const notFound = () => {
+      return (
+         <div>
+            <h1>404 page not found</h1>
+         </div>
+      );
+   };
 
-  return (
-    <Router> 
-      <div className="App">
+   return (
+      <Router>
+         <div className='App'>
+            <NavBar />
 
-        <NavBar />
-
-        <Switch>
-          <Route path="/" exact component={FlowGrid} />
-          <Route path="/create" exact component={AdTemplate} />
-          <Route path="/inventory/:id/edit" component={EditView} />
-          <Route path="/inventory/:id" component={DetailedView} />
-          <Route path="/inventory" exact component={AdGrid} />
-          <Route path="/create/flight" exact component={NavBar} />
-          <Route path="/report" exact component={ShowReport} />
-          <Route path="*" component={notFound} />
-        </Switch>
-
-      </div>
-    </Router>
-  );
+            <Switch>
+               <Route path='/' exact component={FlowGrid} />
+               <Route path='/create' exact component={AdTemplate} />
+               <Route path='/inventory/:id/edit' component={EditView} />
+               <Route path='/inventory/:id' component={DetailedView} />
+               <Route path='/inventory' exact component={AdGrid} />
+               <Route path='/create/flight' exact component={NavBar} />
+               <Route path='/report' exact component={ShowReport} />
+               <Route path='*' component={notFound} />
+            </Switch>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
--- frontend/src/component/ShowReport.jsx
+++ frontend/src/component/ShowReport.jsx
@@ -1,57 +1,75 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function ShowReport(props){
-    const [reportDesktop, updateReportDesktop] = useState({
-        impressions: 0,
-        clicks: 0, 
-        conversions: 0,
-    })
+export default function ShowReport(props) {
+   const [reportDesktop, updateReportDesktop] = useState({
+      impressions: 0,
+      clicks: 0,
+      conversions: 0,
+   });
 
-    const [reportMobile, updateReportMobile] = useState({
-        impressions: 0,
-        clicks: 0, 
-        conversions: 0,
-    })
+   const [reportMobile, updateReportMobile] = useState({
+      impressions: 0,
+      clicks: 0,
+      conversions: 0,
+   });
 
-    const reportPath = 'localhost:3000/reporting'
+   const reportPath = "localhost:3000/reporting";
 
-    axios.get(reportPath + '/desktop')
-        .then((res) => {
-            updateReportDesktop({
-                impressions: res.params.impressions,
-                clicks: res.params.clicks,
-                conversions: res.params.conversions,
-            })
-        })
-        .catch((err) => console.error(err))
-
-    axios.get(reportPath + '/mobile')
-    .then((res) => {
-        updateReportMobile({
+   axios
+      .get(reportPath + "/desktop")
+      .then((res) => {
+         updateReportDesktop({
             impressions: res.params.impressions,
             clicks: res.params.clicks,
             conversions: res.params.conversions,
-        })
-    })
-    .catch((err) => console.error(err))
+         });
+      })
+      .catch((err) => console.error(err));
 
-    return (
-        <div>    
-            <h1 style={{margin: '20px 0 20px 10px', color: '#2B6CB3'}}>Statistics by Flight:</h1>
-            <div style={{margin: '10px', backgroundColor: '#2B6CB3', padding: '10px', color: '#fff'}}>
-                <h2>Desktop</h2>
-                <p>Impressions: {reportDesktop.impressions}</p>
-                <p>Clicks: {reportDesktop.clicks}</p>
-                <p>Conversions: {reportDesktop.conversions}</p>
-            </div>
+   axios
+      .get(reportPath + "/mobile")
+      .then((res) => {
+         updateReportMobile({
+            impressions: res.params.impressions,
+            clicks: res.params.clicks,
+            conversions: res.params.conversions,
+         });
+      })
+      .catch((err) => console.error(err));
 
-            <div style={{margin: '10px', backgroundColor: '#2B6CB3', padding: '10px', color: '#fff'}}>
-                <h2>Mobile</h2>
-                <p>Impressions: {reportMobile.impressions}</p>
-                <p>Clicks: {reportMobile.clicks}</p>
-                <p>Conversions: {reportMobile.conversions}</p>
-            </div>
-        </div>
-    )
+   return (
+      <div>
+         <h1 style={{ margin: "20px 0 20px 10px", color: "#2B6CB3" }}>
+            Statistics by Flight:
+         </h1>
+         <div
+            style={{
+               margin: "10px",
+               backgroundColor: "#2B6CB3",
+               padding: "10px",
+               color: "#fff",
+            }}
+         >
+            <h2>Desktop</h2>
+            <p>Impressions: {reportDesktop.impressions}</p>
+            <p>Clicks: {reportDesktop.clicks}</p>
+            <p>Conversions: {reportDesktop.conversions}</p>
+         </div>
+
+         <div
+            style={{
+               margin: "10px",
+               backgroundColor: "#2B6CB3",
+               padding: "10px",
+               color: "#fff",
+            }}
+         >
+            <h2>Mobile</h2>
+            <p>Impressions: {reportMobile.impressions}</p>
+            <p>Clicks: {reportMobile.clicks}</p>
+            <p>Conversions: {reportMobile.conversions}</p>
+         </div>
+      </div>
+   );
 }
