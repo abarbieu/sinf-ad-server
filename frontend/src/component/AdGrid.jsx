1784567c34f93ff9737b8b1e407dce3fde6af578
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-11-29 15:42:55 -0800

    Styled, 3 spaces

--- frontend/src/component/AdGrid.jsx
+++ frontend/src/component/AdGrid.jsx
@@ -37,58 +37,68 @@ export default function AdGrid(props) {
          },
       ];
 
-    const getItems = async () => {
-        // axios.get('api/inventory')
-        // .then((res) => {
-        //     return res.data.map((ad) => {
-        //        setInv(inv.append(ad));
-        //     });
-        //  })
-        //  .catch((err) => {
-        //     console.error(err);
-        //     console.log('Could not load data')
-        //  })
-        const items = [
+      const getItems = async () => {
+         // axios.get('api/inventory')
+         // .then((res) => {
+         //     return res.data.map((ad) => {
+         //        setInv(inv.append(ad));
+         //     });
+         //  })
+         //  .catch((err) => {
+         //     console.error(err);
+         //     console.log('Could not load data')
+         //  })
+         const items = [
             {
-                adName: 'header 1',
-                adId: '102931028',
-                mainText: 'main',
-                subText: 'sub',
-                linkText: 'link text',
-                linkLoc: '#',
-                flightId: 'Mobile',
-                image: placeholder
+               adName: "header 1",
+               adId: "102931028",
+               mainText: "main",
+               subText: "sub",
+               linkText: "link text",
+               linkLoc: "#",
+               flightId: "Mobile",
+               image: placeholder,
             },
             {
-                adName: 'header 2',
-                adId: '534514312',
-                mainText: 'main',
-                subText: 'sub',
-                linkText: 'link text',
-                linkLoc: '#',
-                flightId: 'Desktop',
-                image: placeholder
+               adName: "header 2",
+               adId: "534514312",
+               mainText: "main",
+               subText: "sub",
+               linkText: "link text",
+               linkLoc: "#",
+               flightId: "Desktop",
+               image: placeholder,
             },
             {
-                adName: 'header 3',
-                adId: '830170283',
-                mainText: 'main',
-                subText: 'sub',
-                linkText: 'link text',
-                linkLoc: '#',
-                flightId: 'Mobile',
-                image: placeholder
-            }
-        ]
+               adName: "header 3",
+               adId: "830170283",
+               mainText: "main",
+               subText: "sub",
+               linkText: "link text",
+               linkLoc: "#",
+               flightId: "Mobile",
+               image: placeholder,
+            },
+         ];
 
-        setInv(items)
-    }
+         setInv(items);
+      };
 
-    return (
-        <div style={{width: '90%', position: 'absolute', top: '150px', left: '5%' , display: 'flex', flexDirection: 'column'}}>
-            {inv.map(v => (
-                <AdPreview key={v.adId} adObj={v} />
+      return (
+         <div
+            style={{
+               width: "90%",
+               position: "absolute",
+               top: "150px",
+               left: "5%",
+               display: "flex",
+               flexDirection: "column",
+            }}
+         >
+            {inv.map((v) => (
+               <AdPreview key={v.adId} adObj={v} />
             ))}
-        </div>
-        )
+         </div>
+      );
+   };
 }
